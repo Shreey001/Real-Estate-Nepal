7fa69cd64eeccbf65e706988d3aecead49e7dabf
Shreey001 <shreejanid123@gmail.com>
2025-04-11 15:44:17 +0545

    Update production API URL in .env.production and apiRequest.js to point to the new backend endpoint

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiRequest = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://real-estate-api-six.vercel.app/api"
+      ? "https://real-estate-backend-indol.vercel.app/api"
       : "http://localhost:4000/api",
   withCredentials: true,
 });
